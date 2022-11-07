a9de650f670177db6354e5890baf7681d13f8e2c
LiuShihao <liush99@foxmail.com>
2022-11-07 15:09:46 +0800

    first commit

---
+++ src/main/java/com/lsh/Master.java
@@ -0,0 +1,12 @@
+package com.lsh;
+
+/**
+ * @Author: LiuShihao
+ * @Date: 2022/11/7 15:08
+ * @Desc:
+ */
+public class Master {
+    public static void main(String[] args) {
+        System.out.println("this is master bance");
+    }
+}
